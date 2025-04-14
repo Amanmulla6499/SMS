ee31ace523978ec491b4f946110b6096603f2d3b
Amanmulla6499 <amanmulla6499>
2025-04-14 11:17:34 +0530

    14-04-2025

--- src/main/java/com/example/sms/entity/Authority.java
+++ src/main/java/com/example/sms/entity/Authority.java
@@ -18,6 +18,8 @@ public class Authority implements GrantedAuthority {
 	private static final long serialVersionUID = 1L;
 	private String authority;
 
+	
+	
 	@Override
 	public String getAuthority() {
 		return this.authority;
--- src/main/java/com/example/sms/entity/Role.java
+++ src/main/java/com/example/sms/entity/Role.java
@@ -14,4 +14,6 @@ public class Role {
 
 	@Id
 	private String role;
+	
+	
 }
